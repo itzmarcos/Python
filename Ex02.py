5c291d407619638e9de2b4ca382541b1cb5c82f9
itzmarcos <marcozfuwa@gmail.com>
2024-06-01 12:45:43 -0300

    feat: add con

--- Ex02.py
+++ Ex02.py
@@ -7,12 +7,25 @@
 # resultado = multiplicar(2, 3, 4)
 # #print(f"O valor da variável resultado é: {resultado}")
 
-def verificar_paridade(numero):
-    if numero % 2 == 0:
-        return "par"
-    else:
-        return "ímpar"
+# def verificar_paridade(numero):
+#     if numero % 2 == 0:
+#         return "par"
+#     else:
+#         return "ímpar"
 
-numero = 9
-resultado = verificar_paridade(numero)
-print(f"O número {numero} é {resultado}.")
+# numero = 9
+# resultado = verificar_paridade(numero)
+# print(f"O número {numero} é {resultado}.")
+
+
+
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+print(duplicar(2))
+print(triplicar(3))
