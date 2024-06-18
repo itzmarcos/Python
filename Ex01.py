39bab8cec82c2ba0389a4459cac53eedbfbb18f7
itzmarcos <marcozfuwa@gmail.com>
2024-06-18 15:51:01 -0300

    feat:set

--- Ex01.py
+++ Ex01.py
@@ -1,30 +1,44 @@
-import os
-lista = []
+# import os
+# lista = []
 
-while True:
-    print('Selecione uma opção')
-    opção = input('[i]nserir', '[a]pagar', '[l]istar')
-    if opção == 'i':
-        nome = input('Nome: ')
-        list.append(nome)
-    elif opção == 'a':
-        indice_str = input(
-            'Escolha o índice para apagar: '
-        )
-        try:
-            indice = int(indice_str)
-            del lista[indice]
-        except ValueError:
-            print('Por favor digite numero int.')
-        except IndexError:
-            print('índice não existe na lista')
-        except Exception:
-            print('Erro desconhecido')
-    elif opção == 'l':
-        os.system('clear')
+# while True:
+#     print('Selecione uma opção')
+#     opção = input('[i]nserir', '[a]pagar', '[l]istar')
+#     if opção == 'i':
+#         nome = input('Nome: ')
+#         list.append(nome)
+#     elif opção == 'a':
+#         indice_str = input(
+#             'Escolha o índice para apagar: '
+#         )
+#         try:
+#             indice = int(indice_str)
+#             del lista[indice]
+#         except ValueError:
+#             print('Por favor digite numero int.')
+#         except IndexError:
+#             print('índice não existe na lista')
+#         except Exception:
+#             print('Erro desconhecido')
+#     elif opção == 'l':
+#         os.system('clear')
 
-        if len(lista) == 0:
-            print('Nada para lsitar')
+#         if len(lista) == 0:
+#             print('Nada para lsitar')
 
-        for i, nome in enumerate(lista):
-            print(i,nome)
+#         for i, nome in enumerate(lista):
+#             print(i,nome)
+
+lista = [ 
+    'a', 1, 1.1, True, [0, 1, 2], (1, 2), {0, 1}, {'nome': 'Luiz'}
+]
+for item in lista:
+    if isinstance (item , set):
+        print('SET')
+        item.add(5)
+        print(item, isinstance(item, set))
+    elif isinstance(item, str):
+        print('STR')
+        print(item.upper)
+    else:
+        print('Outro')
